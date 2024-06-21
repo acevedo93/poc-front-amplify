581dac0993b90056a6c39971839ecc920ae74a38
tecKaleAnders <143652890+tecKaleAnders@users.noreply.github.com>
2024-06-20 22:06:59 -0500

    add api integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Amplify } from "aws-amplify"
+import outputs from "../../amplify_outputs.json"
+import { ApiError, get } from 'aws-amplify/api'
+
+
+Amplify.configure(outputs)
+
+const existingConfig = Amplify.getConfig()
+Amplify.configure({
+
+  ...existingConfig,
+  API: {
+    ...existingConfig.API,
+    REST: outputs.custom.API
+  }
+
+})
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +29,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'amplifyFrontend';
+
+  constructor() {
+    this.getItem()
+  }
+
+  async getItem(): Promise<void> {
+
+    try {
+      const restOperation = get({
+        apiName: "MyRestApiExample",
+        path: 'items',
+      })
+
+      const response = await restOperation.response
+      console.log("OPERATION END", response)
+      console.log(await response.body.json())
+
+    } catch (error) {
+      if (error instanceof ApiError) {
+
+        console.log("ERROR ON RESPONSE", error)
+      }
+    }
+  }
 }
